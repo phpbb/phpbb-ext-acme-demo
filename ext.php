e4607cd2d0ea00bd2150ef8c3845368bf9895526
Joas Schilling <nickvergessen@gmx.de>
2013-11-05 18:38:55 +0100

    Add composer.json and ext.php so we can enable the extension

---
+++ ext.php
@@ -0,0 +1,22 @@
+<?php
+/**
+*
+* @package phpBB Extension - Acme Demo
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace acme\demo;
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+class ext extends \phpbb\extension\base
+{
+}
