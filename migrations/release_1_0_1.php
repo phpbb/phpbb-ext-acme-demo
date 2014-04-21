9ef4dbaafc7c414a0522b4de75ce20e1661fc896
Joas Schilling <nickvergessen@gmx.de>
2014-04-21 21:44:30 +0200

    Add a simple database schema test

---
+++ migrations/release_1_0_1.php
@@ -0,0 +1,57 @@
+<?php
+/**
+*
+* @package phpBB Extension - Acme Demo
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace acme\demo\migrations;
+
+class release_1_0_1 extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return $this->db_tools->sql_column_exists($this->table_prefix . 'users', 'user_acme');
+	}
+
+	static public function depends_on()
+	{
+		return array('\acme\demo\migrations\release_1_0_0');
+	}
+
+	public function update_schema()
+	{
+		return array(
+			'add_tables'		=> array(
+				$this->table_prefix . 'acme_demo'	=> array(
+					'COLUMNS'		=> array(
+						'acme_id'			=> array('UINT', NULL, 'auto_increment'),
+						'acme_name'			=> array('VCHAR:255', ''),
+					),
+					'PRIMARY_KEY'	=> 'acme_id',
+				),
+			),
+			'add_columns'	=> array(
+				$this->table_prefix . 'users'			=> array(
+					'user_acme'				=> array('UINT', 0),
+				),
+			),
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+			'drop_columns'	=> array(
+				$this->table_prefix . 'users'			=> array(
+					'user_acme',
+				),
+			),
+			'drop_tables'		=> array(
+				$this->table_prefix . 'acme_demo',
+			),
+		);
+	}
+}
---
+++ tests/dbal/simple_test.php
@@ -0,0 +1,29 @@
+<?php
+/**
+*
+* @package phpBB Extension - Acme Demo
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace acme\demo\tests\dbal;
+
+class simple_test extends \phpbb_database_test_case
+{
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
+
+	public function getDataSet()
+	{
+		return $this->createXMLDataSet(dirname(__FILE__) . '/fixtures/config.xml');
+	}
+
+	public function test_column()
+	{
+		$this->db = $this->new_dbal();
+		$db_tools = new \phpbb\db\tools($this->db);
+		$this->assertTrue($db_tools->sql_column_exists(USERS_TABLE, 'user_acme'));
+		$this->assertFalse($db_tools->sql_column_exists(USERS_TABLE, 'user_acme_demo'));
+	}
+}
