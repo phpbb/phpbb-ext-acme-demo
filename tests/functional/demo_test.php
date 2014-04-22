68364538e808ee22ae335156b843ccba69cf8897
Joas Schilling <nickvergessen@gmx.de>
2014-04-22 08:49:26 +0200

    Compare to extensions language in functional test
    Fix #2

--- tests/functional/demo_test.php
+++ tests/functional/demo_test.php
@@ -18,6 +18,8 @@ class demo_test extends \acme\demo\tests\framework\functional_test_case
 	{
 		$crawler = self::request('GET', 'app.php/demo/acme');
 		$this->assertContains('acme', $crawler->filter('h2')->text());
+		$this->assertContainsLang(array('DEMO_HELLO', 'acme'), $crawler->filter('h2')->text());
+		$this->assertNotContainsLang(array('DEMO_GOODBYE', 'acme'), $crawler->filter('h2')->text());
 	}
 
 	public function test_demo_world()
