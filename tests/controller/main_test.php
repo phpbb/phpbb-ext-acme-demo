ae743febe5fd04d301df436f04dd132ed6e558dc
Matt Friedman <maf675@gmail.com>
2016-02-27 12:00:24 -0800

    Remove mock test files

--- tests/controller/main_test.php
+++ tests/controller/main_test.php
@@ -23,11 +23,33 @@ class main_test extends \phpbb_test_case
 	 */
 	public function test_handle($status_code, $page_content)
 	{
+		// Mocks are dummy implementations that provide the API of components we depend on //
+		/** @var \phpbb\template\template $template Mock the template class */
+		$template = $this->getMockBuilder('\phpbb\template\template')
+			->disableOriginalConstructor()
+			->getMock();
+
+		/** @var \phpbb\user $user Mock the user class */
+		$user = $this->getMock('\phpbb\user', array(), array('\phpbb\datetime'));
+
+		/** @var \phpbb\controller\helper $controller_helper Mock the controller helper class */
+		$controller_helper = $this->getMockBuilder('\phpbb\controller\helper')
+			->disableOriginalConstructor()
+			->getMock();
+
+		// Set the expected output of the controller_helper->render() method
+		$controller_helper->expects($this->any())
+			->method('render')
+			->willReturnCallback(function ($template_file, $page_title = '', $status_code = 200, $display_online_list = false) {
+				return new \Symfony\Component\HttpFoundation\Response($template_file, $status_code);
+			});
+
+		// Instantiate the acme demo controller
 		$controller = new \acme\demo\controller\main(
 			new \phpbb\config\config(array()),
-			new \acme\demo\tests\mock\controller_helper(),
-			new \acme\demo\tests\mock\template(),
-			new \acme\demo\tests\mock\user()
+			$controller_helper,
+			$template,
+			$user
 		);
 
 		$response = $controller->handle('test');
